6eb5310e637cda4f4f4804c335c99b5c542a57f7
Ed Wallitt <edwallitt@icloud.com>
2020-04-30 08:59:38 +0100

    update the update data

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ sns.set(style="darkgrid")
 
 MERGED_DATA_LOCATION = './data/ppe-merged-responses.csv'
 MAPBOX_API_KEY = os.environ.get('MAPBOX_TOKEN')
-LAST_UPDATE = '29th April, 1115'
+LAST_UPDATE = '30th April, 0900'
 
 
 @st.cache()
